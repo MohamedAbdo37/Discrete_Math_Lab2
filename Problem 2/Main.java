98435be0ac2d52c7a9a10d266cbda6b4e0369701
zoaa3054 <121523599+zoaa3054@users.noreply.github.com>
2023-11-08 13:36:47 +0200

    Add files via upload

---
+++ Problem 2/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // reading expression
+        System.out.println("Enter expression: ");
+        String input = scanner.nextLine();
+        String res = new String();
+
+        // Reading values
+        String values = scanner.nextLine().replace(" ", "");// reading values string and removing spaces
+        values = values.replace("=", "");// removing "=" signs
+        values = values.replace("and", ",");// remplacing "and" with ","
+        String[] valuesArray = values.split(",");
+        // creating an expression object
+        Expression expression = new Expression(input, valuesArray);
+
+        // getting representation
+        try {
+            res = expression.getRepresentation();
+        } catch (Exception e) {
+            System.out.println("Wrong expression");
+            return;
+        }
+        // parsing opperand values in the expression representation
+        try {
+            expression.setRepresentation(res);
+        } catch (Exception e) {
+            System.out.println("Wrong expression");
+            return;
+        }
+
+        LogicalExpressionSolver logicStatement = new LogicalExpressionSolver();
+        // estimamting result
+        boolean boolResult = true;
+        try {
+            boolResult = logicStatement.evaluateExpression(expression);
+        } catch (Exception e) {
+            System.out.println("Wrong expression");
+            return;
+        }
+        System.out.println(boolResult);
+    }
+}
